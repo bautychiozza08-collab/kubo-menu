f17784503538482fac70fc3148432b57fb133bee
Bautista <bautychiozza08@gmail.com>
2026-06-12 12:08:58 -0300

    add menu editable

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-const items = document.querySelectorAll(".item");
-const editorPanel = document.getElementById("editorPanel");
-const priceEditor = document.getElementById("priceEditor");
-
 const isAdmin = window.location.search.includes("admin=true");
 
 const editButton = document.querySelector(".edit-btn");
+const items = document.querySelectorAll(".item");
+const editorPanel = document.getElementById("editorPanel");
+const priceEditor = document.getElementById("priceEditor");
 
 if (!isAdmin && editButton) {
   editButton.style.display = "none";
 }
 
-let prices = JSON.parse(localStorage.getItem("kuboPrices")) || {};
+let savedMenu = JSON.parse(localStorage.getItem("kuboMenuData")) || {};
 
-function renderPrices() {
+function renderMenu() {
   items.forEach(item => {
-    const name = item.dataset.name;
+    const originalName = item.dataset.name;
     const span = item.querySelector("span");
 
-    span.textContent = prices[name] || "$ ______";
+    const saved = savedMenu[originalName];
+
+    if (saved?.nombre) {
+      item.childNodes[0].textContent = saved.nombre + " ";
+    }
+
+    span.textContent = saved?.precio || "$ ______";
   });
 }
 
@@ -25,13 +30,21 @@ function openEditor() {
   priceEditor.innerHTML = "";
 
   items.forEach(item => {
-    const name = item.dataset.name;
+    const originalName = item.dataset.name;
+    const saved = savedMenu[originalName] || {};
 
     priceEditor.innerHTML += `
       <label>
-        ${name}
-        <input type="text" value="${prices[name] || ""}" data-name="${name}">
+        Producto
+        <input type="text" value="${saved.nombre || originalName}" data-original="${originalName}" data-type="nombre">
       </label>
+
+      <label>
+        Precio
+        <input type="text" value="${saved.precio || ""}" data-original="${originalName}" data-type="precio">
+      </label>
+
+      <hr>
     `;
   });
 
@@ -46,13 +59,20 @@ function savePrices() {
   const inputs = priceEditor.querySelectorAll("input");
 
   inputs.forEach(input => {
-    prices[input.dataset.name] = input.value;
+    const originalName = input.dataset.original;
+    const type = input.dataset.type;
+
+    if (!savedMenu[originalName]) {
+      savedMenu[originalName] = {};
+    }
+
+    savedMenu[originalName][type] = input.value;
   });
 
-  localStorage.setItem("kuboPrices", JSON.stringify(prices));
+  localStorage.setItem("kuboMenuData", JSON.stringify(savedMenu));
 
-  renderPrices();
+  renderMenu();
   closeEditor();
 }
 
-renderPrices();
+renderMenu();
